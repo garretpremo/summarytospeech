0778dadaca40aa7954d44a5a4e743345470a50f7
Lucas Volle <lrvolle@gmail.com>
2016-10-08 19:53:33 -0400

    Added context menu feature for selected text and icons

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ window.onload = function() { //SMMRY API abstraction
         SM_API_URL = "http://api.smmry.com/"
         SM_API_KEY = "0C05A0A3E9" // Mandatory, N represents your registered API key.
             // SM_URL = X // Mandatory, X represents the webpage to summarize.
-        SM_LENGTH = 5 // Optional, N represents the number of sentences returned, default is 7 
+        SM_LENGTH = 5 // Optional, N represents the number of sentences returned, default is 7
             // SM_KEYWORD_COUNT = N // Optional, N represents how many of the top keywords to return
             // SM_QUOTE_AVOID     // Optional, summary will not include quotations
             // SM_WITH_BREAK      // Optional, summary will contain string [BREAK] between each
@@ -48,5 +48,19 @@ window.onload = function() { //SMMRY API abstraction
         // alert(summary);
         // while(1);
     });
-    // alert("opened page");
+    //alert("opened page");
+
+    //recieve text from context menu click
+    function setSelectionText(info) {
+      console.log(info);
+      //update text area
+      $('textarea#textarea1').val(info);
+      $('textarea#textarea1').trigger('autoresize');
+      //call smmry
+      var summary = getTextSMMRY(info);
+    }
+
+    //sent message to backgroundjs asking for selectionText
+    chrome.runtime.sendMessage({msg: 'selectionText'}, setSelectionText);
+
 }
