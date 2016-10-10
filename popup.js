73fb9a3759355ba8e72630e70255cb72edb79f21
Jonathan Elias Caicedo <Jonathan@jcaicedo.tk>
2016-10-10 14:19:32 -0400

    Simplify speechUtteranceChunker to remove speech ordering issues.

--- popup.js
+++ popup.js
@@ -7,62 +7,21 @@ window.onload = function() { //SMMRY API abstraction
 
     document.title = "Summary to Speech";
 
-    var speechUtteranceChunker = function (utt, settings, callback) {
-        settings = settings || {};
-        var newUtt;
-        var txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
-        if (utt.voice && utt.voice.voiceURI === 'native') { // Not part of the spec
-            newUtt = utt;
-            newUtt.text = txt;
-            newUtt.addEventListener('end', function () {
-                if (speechUtteranceChunker.cancel) {
-                    speechUtteranceChunker.cancel = false;
-                }
-                if (callback !== undefined) {
-                    callback();
-                }
-            });
-        }
-        {
-            var chunkLength = (settings && settings.chunkLength) || 160;
-            var pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
-            var chunkArr = txt.match(pattRegex);
+    var speechUtteranceChunker = function (utt, chunkSize, callback) {
+      var chunkLength = chunkSize;
+      var pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
 
-            if (chunkArr[0] === undefined || chunkArr[0].length <= 2) {
-                //call once all text has been spoken...
-                if (callback !== undefined) {
-                    callback();
-                }
-                return;
-            }
-            var chunk = chunkArr[0];
-            newUtt = new SpeechSynthesisUtterance(chunk);
-            var x;
-            for (x in utt) {
-                if (utt.hasOwnProperty(x) && x !== 'text') {
-                    newUtt[x] = utt[x];
-                }
-            }
-            newUtt.addEventListener('end', function () {
-                if (speechUtteranceChunker.cancel) {
-                    speechUtteranceChunker.cancel = false;
-                    return;
-                }
-                settings.offset = settings.offset || 0;
-                settings.offset += chunk.length - 1;
-                speechUtteranceChunker(utt, settings, callback);
-            });
+      var arr = [];
+      var txt = utt.text
+      while (txt.length > 0) {
+            arr.push(txt.match(pattRegex)[0]);
+            txt = txt.substring(arr[arr.length - 1].length);
         }
-
-        if (settings.modifier) {
-            settings.modifier(newUtt);
-        }
-        console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
-        //placing the speak invocation inside a callback fixes ordering and onend issues.
-        setTimeout(function () {
-            newUtt.voice = utt.voice;
-            speechSynthesis.speak(newUtt);
-        }, 0);
+        $.each(arr, function () {
+            var u = new SpeechSynthesisUtterance(this.trim());
+            u.voice = utt.voice
+            window.speechSynthesis.speak(u);
+        });
     };
 
     function handleTextSMMRYAsync(res) {
@@ -84,7 +43,7 @@ window.onload = function() { //SMMRY API abstraction
                 var utterance = new SpeechSynthesisUtterance(res.sm_api_content);
                 var voices = window.speechSynthesis.getVoices();
                 utterance.voice = voices.filter(function(voice) { return voice.name == "Google US English"; })[0];
-                speechUtteranceChunker(utterance, {chunkLength: 160}, function () {
+                speechUtteranceChunker(utterance, 160, function () {
                   //some code to execute when done
                   console.log('Finished speaking.');
                 });
