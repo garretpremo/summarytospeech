583a451b04091d88a969da44da53cbf3cf20bc34
garretpremo <garretpremo@gmail.com>
2016-10-29 12:33:54 -0400

    cleaned up background.js

--- background.js
+++ background.js
@@ -8,10 +8,6 @@ chrome.contextMenus.create( {
 });
 
 function sendTextToPopup(info, tab) {
-  //var mouseEvent = new MouseEvent('event');
-  //open popup
-  //window.open("popup.html", "SummarizeAndSay", "width=360,height=365,status=no,scrollbars=no,resizable=no");
-
   var popupData = {
     url: chrome.extension.getURL("popup.html"),
     width: 360,
@@ -23,18 +19,19 @@ function sendTextToPopup(info, tab) {
     popupData.width = 370;
     popupData.height= 330;
   }
-  // if (event) {
-  //   popupData.left = event.screenX;
-  //   popupData.top = event.screenY;
-  // }
+
   chrome.windows.create(popupData);
+
   //send selectionText to popup when requested
   chrome.runtime.onMessage.addListener(function listener(reqest, sender, sendResponse) {
+
     //chrome.tabs.getCurrent(function(tab){ console.log(tab.id); })
     if (reqest.msg == 'selectionText') {
+
       //alert(info.selectionText);
       sendResponse(info.selectionText);
     }
+
     chrome.runtime.onMessage.removeListener(listener);
   });
 
